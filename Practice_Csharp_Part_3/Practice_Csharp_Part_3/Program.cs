590150ab018c5f4504f7ff1ac06c27c352cc00b8
KimHJo <guswh1012@gmail.com>
2019-07-29 14:42:47 +0900

    1.List<T> class & Ramda example code.

--- Practice_Csharp_Part_3/Practice_Csharp_Part_3/Program.cs
+++ Practice_Csharp_Part_3/Practice_Csharp_Part_3/Program.cs
@@ -1,9 +1,56 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Practice_Csharp_Part_3 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            
+            // City list
+            var list = new List<string> {
+                "Seoul", "New Delhi", "Bangkok", "London", "Paris", "Canberra", "Hong kong",
+            };
+
+            // Exists
+            var exists1 = list.Exists(s => s[0] == 'A');
+            var exists2 = list.Exists(s => s.Contains('a'));
+            Console.WriteLine(exists1);
+            Console.WriteLine(exists2);
+            Console.WriteLine("--------------------");
+
+            // Find
+            var name = list.Find(s => s.Length == 6);
+            Console.WriteLine(name);
+            Console.WriteLine("--------------------");
+
+            // FindIndex
+            var index = list.FindIndex(s => s == "London");
+            Console.WriteLine(index);
+            Console.WriteLine("--------------------");
+
+            // FindAll
+            var names = list.FindAll(s => s.Length >= 3);
+            foreach (var n in names) {
+                Console.WriteLine(n);
+            }
+            Console.WriteLine("--------------------");
+
+            // RemoveAll
+            var removedCount = list.RemoveAll(s => s.Contains("on"));
+            Console.WriteLine(removedCount);
+            Console.WriteLine("--------------------");
+
+            // ForEach
+            list.ForEach(s => Console.WriteLine(s));
+            //list.ForEach(Console.WriteLine);
+            Console.WriteLine("--------------------");
+
+            // ConvertAll
+            var lowerList = list.ConvertAll(s => s.ToLower());
+            lowerList.ForEach(Console.WriteLine);
+
+            var upperList = list.ConvertAll(s => s.ToUpper());
+            upperList.ForEach(Console.WriteLine);
+            Console.WriteLine("--------------------");
         }
     }
 }
