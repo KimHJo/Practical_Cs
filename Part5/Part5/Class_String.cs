3105a630df28b63fb25131d41c49c8a52ae35da2
KimHJo <guswh1012@gmail.com>
2019-07-29 15:38:29 +0900

    1.Make new project to practic Part5 Class String Methode

---
+++ Part5/Part5/Class_String.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Part5 {
+    class Class_String {
+
+        public void Showresult() {
+            var str1 = "Windows";
+            var str2 = "WINDOWS";
+
+            if (String.Compare(str1, str2, ignoreCase: true) == 0)
+                Console.WriteLine("같다.");
+            else
+                Console.WriteLine("다르다.");
+        }
+    }
+}
---
+++ Part5/Part5/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Part5 {
+    class Program {
+        static void Main(string[] args) {
+            Class_String cs = new Class_String();
+
+            cs.Showresult();
+        }
+    }
+}
