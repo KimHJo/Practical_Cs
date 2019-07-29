7a2a86ae49863c379e41d90bf40b33211719c8de
KimHJo <guswh1012@gmail.com>
2019-07-29 16:56:26 +0900

    Practice All-Digit, IndexOf, Substring, Trim, Remove, Insert, Replace Methode in String Class

--- Part5/Part5/Program.cs
+++ Part5/Part5/Program.cs
@@ -16,6 +16,29 @@ namespace Part5 {
 
             var str3 = "The quick brown fox jumps over the lazy dog";
             cs.ShowAnyResult(str3, 't');
+
+            var str4 = "1234124124699";
+            cs.ShowAllDigitResult(str4);
+
+            var str5 = "Novelist=김만중;BestWork=구운몽";
+            cs.ShowIndexOfResult(str5, "BestWork=");
+
+            cs.ShowSubStringResult(str5, "BestWork=");
+
+            var str6 = "Novelist=김만중;BestWork=구운몽;Born=1666";
+            cs.ShowSubString2Result(str6, "BestWork=");
+
+            var str7 = " non-whitespace characters ";
+            cs.ShowTrimResult(str7);
+
+            var str8 = "01234ABCD56789";
+            cs.ShowRemoveResult(str8, 5, 3);
+
+            var str9 = "01234";
+            cs.ShowInsertResult(str9, 2, "abc");
+
+            var str10 = "I hope you could come with us";
+            cs.ShowReplaceResult(str10, "hope", "wish");
         }
     }
 }
