9fadc7e61acee93facb425f850b358ba4ebb641a
KimHJo <guswh1012@gmail.com>
2019-07-29 15:54:45 +0900

    Practice Methode Compare, StartWith, Contains, Any in String Class.

--- Part5/Part5/Class_String.cs
+++ Part5/Part5/Class_String.cs
@@ -1,18 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Part5 {
     class Class_String {
 
-        public void Showresult() {
-            var str1 = "Windows";
-            var str2 = "WINDOWS";
-
-            if (String.Compare(str1, str2, ignoreCase: true) == 0)
+        public void ShowCompareResult(string str1, string str2, bool ignorecase = true) {
+            Console.WriteLine("---- Show Compare Methode Result ----");
+            if (String.Compare(str1, str2, ignoreCase: ignorecase) == 0)
                 Console.WriteLine("같다.");
             else
                 Console.WriteLine("다르다.");
+        }
+
+        public void ShowStartWithResult(string msg, string str) {
+            Console.WriteLine("---- Show StartWith Methode Result ----");
+            if (str.StartsWith(str))
+                Console.WriteLine(str + "로 시작합니다");
+        }
+
+        public void ShowContainsResult(string msg, string str) {
+                        Console.WriteLine("---- Show Contains Methode Result ----");
+            if (str.Contains(str))
+                Console.WriteLine(str + "를 포함합니다.");
+        }
+
+        public void ShowAnyResult(string msg, char str) {
+            Console.WriteLine("---- Show Any Methode Result ----");
+            var isExists = msg.Any(c => Char.IsLower(str));
+            Console.WriteLine(isExists);
         }
     }
 }
--- Part5/Part5/Program.cs
+++ Part5/Part5/Program.cs
@@ -3,9 +3,19 @@
 namespace Part5 {
     class Program {
         static void Main(string[] args) {
-            Class_String cs = new Class_String();
+            var cs = new Class_String();
 
-            cs.Showresult();
+
+            cs.ShowCompareResult("Window", "WINDOW");
+
+            var str = "Visual Basic C#";
+            cs.ShowStartWithResult(str, "Visual");
+
+            var str2 = "C# Program is funny!";
+            cs.ShowContainsResult(str2, "Program");
+
+            var str3 = "The quick brown fox jumps over the lazy dog";
+            cs.ShowAnyResult(str3, 't');
         }
     }
 }
