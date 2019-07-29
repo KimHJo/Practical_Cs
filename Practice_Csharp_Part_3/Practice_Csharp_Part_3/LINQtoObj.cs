224c226c8e48930149da5de213ab3a74d5d659d0
KimHJo <guswh1012@gmail.com>
2019-07-29 15:29:42 +0900

    1. Practice Delay action, Immedatily action in Linq

--- Practice_Csharp_Part_3/Practice_Csharp_Part_3/LINQtoObj.cs
+++ Practice_Csharp_Part_3/Practice_Csharp_Part_3/LINQtoObj.cs
@@ -14,10 +14,27 @@ namespace Practice_Csharp_Part_3 {
             Console.WriteLine("---- WHERE & SELECT ----");
             var query = list.Where(l => l.Length >= 5)
                             .Select(l => l.ToLower());
-            foreach(var q in query) {
+            foreach (var q in query) {
                 Console.WriteLine(q);
             }
             Console.WriteLine();
+
+            Console.WriteLine("---- 지연실행 ----");
+            query = list.Where(s => s.Length >= 5);
+            foreach (var item in query) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("---- change name Seoul to Busan ----");
+            list[0] = "Busan";
+            foreach(var item in query) {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+            
+            Console.WriteLine("---- ConvertAll ToUpper ----");
+            list = list.ConvertAll(s => s.ToUpper());
+            list.ForEach(Console.WriteLine);
+
         }
     }
 }
